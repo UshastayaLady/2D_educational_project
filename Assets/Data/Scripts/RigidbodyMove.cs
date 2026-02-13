591d393e4a1c1e4be15e9dd14b0ec45c41b3e316
B3441 <1arya11stark@gmail.com>
2026-02-13 15:17:27 +0300

    создание анимации передвижения и прыжка

--- Assets/Data/Scripts/RigidbodyMove.cs
+++ Assets/Data/Scripts/RigidbodyMove.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Animator))]
 public class RigidbodyMove : MonoBehaviour
 {
     private Rigidbody2D rigbody2D;
+    private SpriteRenderer spriteRenderer2D;
+    private Animator animator2D;
 
     [Header("Horizontal Transform")]
     [SerializeField] private float speedGo = 3;
@@ -20,29 +24,48 @@ public class RigidbodyMove : MonoBehaviour
     private bool jumpPressed = false;
     private bool isGrounded = true;
 
+    [Header("Animator")]
+    private const string idleRun_VelosityX = nameof(idleRun_VelosityX);
+    private const string anyJump_VelosityY = nameof(anyJump_VelosityY);
+
 
     void Awake()
     {
-        rigbody2D = this.GetComponent<Rigidbody2D>();
+        rigbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer2D = GetComponent<SpriteRenderer>();
+        animator2D = GetComponent<Animator>();
         groundLayer = LayerMask.GetMask("Ground");
     }
 
     private void Update()
     {
+        DownButton();
+        ChangeAnimation();
+    }
+    private void DownButton()
+    {
         directionX = Input.GetAxis(Horizontal);
         if (Input.GetKeyDown(KeyJump))
         {
             jumpPressed = true;
-        }        
+        }
     }
 
     private void FixedUpdate()
     {
+        Move();
+        FlipX();
+        Jump();       
+    }
+    private void Move()
+    {
         //rigidbody2D.linearVelocity = 
         //    new Vector2(directionX * speed, rigidbody2D.linearVelocityY); - длинный вариант
 
-        rigbody2D.linearVelocityX = directionX * speedGo;
-
+        rigbody2D.linearVelocityX = directionX * speedGo;       
+    }
+    private void Jump()
+    {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
 
         if (jumpPressed && isGrounded)
@@ -52,5 +75,42 @@ public class RigidbodyMove : MonoBehaviour
         }
 
         jumpPressed = false;
+    }
+
+    private void ChangeAnimation()
+    {
+        if (rigbody2D.linearVelocityX == 0)
+        {
+            animator2D.SetFloat(idleRun_VelosityX, -1);
+        }
+        else
+        {
+            animator2D.SetFloat(idleRun_VelosityX, 1);
+        }
+
+        if (rigbody2D.linearVelocityY < 0)
+        {
+            animator2D.SetFloat(anyJump_VelosityY, -1);
+        }
+        else if (rigbody2D.linearVelocityY > 0)
+        {
+            animator2D.SetFloat(anyJump_VelosityY, 1);
+        }
+        else 
+        {
+            animator2D.SetFloat(anyJump_VelosityY, 0);
+        }
+    }
+
+    private void FlipX()
+    {
+        if (directionX < 0)
+        {
+            spriteRenderer2D.flipX = true;
+        }
+        else if (directionX > 0)
+        {
+            spriteRenderer2D.flipX = false;
+        }
     }
 }
